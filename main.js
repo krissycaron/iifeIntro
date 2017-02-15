98fa70308670cf217239d06b30aeac31f5c3dc50
Kristin Caron <krissycaron@gmail.com>
2017-02-14 18:47:37 -0600

    notes

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log("hi");
+// var outside = "global"; 
+
+// function scopeStuff(){ 
+// 	var	inside = "local";
+// 	console.log("outside from inside the function", outside)
+// 	console.log("inside from inside the function", inside)
+// 	// body...
+// }
+
+// console.log("outside from outside the function", outside);//this will run
+// console.log("inside from out side the function", inside);//this will not run becasue it is inside the function "undefined"
+
+// //to get a variable private, stick it inside your function,
+// scopeStuff();
+
+
+// var global_base = 500;
+
+// (function throwAway(){
+// 	global_base+=500;
+// 	console.log(global_base);//1000 
+// }) ();
+
+
+//immiedietly invoked function expression .. 
+
+// function declaration this is just writing the functions
+// function expression var X=1
+
+// throwAway();
+
+//parenthisis () literaly "call the function" or "run the function"
+
+// document.getElementById('cat').addEventListner('click', throwAway) 
+
+//immiediatly invoked function ... just want it to run... 
+
+
+var cats (function(){
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
